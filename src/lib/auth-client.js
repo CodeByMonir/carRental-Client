bd95ec89de5ceb940d3123acb41bcad05a26b2f3
CodeByMonir <monirhossen39682@gmail.com>
2026-05-27 22:27:28 +0600

    added a safety better auth url in auth clients

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,10 +1,8 @@
 import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 const client = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
-    plugins: [
-            jwtClient(),
-    ]
-})
+  baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  plugins: [jwtClient()],
+});
 export const authClient = client
 export const { signIn, signUp, signOut, useSession } = client
